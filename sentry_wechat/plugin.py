78dae92e9312c78b6fcf62fe5cc08a7a7737b17b
huming <ming.hu@mail.jusdascm.com>
2019-12-24 09:45:21 +0800

    ....

--- sentry_wechat/plugin.py
+++ sentry_wechat/plugin.py
@@ -56,7 +56,7 @@ class WechatForm(notify.NotificationConfigurationForm):
 class WechatPlugin(notify.NotificationPlugin):
     author = 'jerry hu'
     author_url = 'https://github.com/jerryhu1234/sentry-wechat'
-    version = sentry-wechat.VERSION
+    version = sentry.VERSION
     description = "Integrates wechat robot."
     resource_links = [
         ('Bug Tracker', 'https://github.com/jerryhu1234/sentry-wechat/issues'),
