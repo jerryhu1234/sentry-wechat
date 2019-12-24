ec2cd3f660655d369423ac62b53397207badc73c
huming <ming.hu@mail.jusdascm.com>
2019-12-24 11:39:37 +0800

    ...

--- setup.py
+++ setup.py
@@ -8,51 +8,40 @@ An extension for Sentry which integrates with qy-Wetchat. It will send
 notifications to qy-wetchat robot.
 
 :copyright: (c) 2019 by jerry hu, see AUTHORS for more details.
-:license: BSD, see LICENSE for more details.
+:license: MIT, see LICENSE for more details.
 """
-from setuptools import setup, find_packages
 
-# for why we need to do this.
-from multiprocessing import util
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-tests_require = [
-]
-
-install_requires = [
-    'sentry>=9.1.1',
-]
-
 setup(
-    name='sentry-wechat',
-    version='1.0.0',
+    name="sentry-wechat",
+    version='0.0.1',
     author='jerry hu',
     author_email='jerryhu_123@163.com',
     url='https://github.com/jerryhu1234/sentry-wechat',
     description='A Sentry extension which integrates with Wechat robot.',
-    # long_description=__doc__,
     long_description=long_description,
-    long_description_content_type="text/markdown",    
-    license='BSD',
-    packages=find_packages(exclude=['tests']),
+    long_description_content_type="text/markdown",
+    license='MIT',
+    keywords='sentry wechat',
+    include_package_data=True,
     zip_safe=False,
-    install_requires=install_requires,
-    tests_require=tests_require,
-    extras_require={'test': tests_require},
-    test_suite='nose.collector',
+    package_dir={'': 'sentry_wechat'},
+    packages=find_packages('sentry_wechat'),
+    install_requires=[
+        'sentry>=9.0.0',
+        'requests',
+    ],
     entry_points={
         'sentry.plugins': [
             'wechat = sentry_wechat.plugin:WechatPlugin'
-        ],
+        ]
     },
-    include_package_data=True,
     classifiers=[
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development'
-    ],
+        'Programming Language :: Python :: 2.7',
+        "License :: OSI Approved :: MIT License",
+    ]
 )
